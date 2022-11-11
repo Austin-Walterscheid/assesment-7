31316b4be4a3da28139d0b3a39e8a30b337bce10
Austin Walterscheid <aus.10walterscheid@gmail.com>
2022-11-11 16:00:04 -0700

    upload

--- functions.js
+++ functions.js
@@ -1,77 +1,70 @@
 //1. sum zero
-let array = [1, 3, 4, 7, 3]
-let array2 = [ 1, 2, 3, 4, 5]
+// runtime O(n)
 function addToZero (arr) {
     for (let i=0; i<arr.length; i++) {
-        const curNum = arr[i];
-        for (let j=i+1; j<arr.length; j++) {
-          if ((curNum - arr[j]) === 0) {
+        const curNum = arr[i]
+        for (let j=0; j<arr.length; j++) {
+            let bothNums = arr[j]
+          if (curNum + bothNums === 0 && i !==j) {
             return true
-          }else{
-            return false
+          
           }
         }
       }
+      return false
 }
 
-// console.log(addToZero(array))
-// console.log(addToZero(array2))
+// console.log(addToZero([1, 3, 4, 7, -3]))
+// console.log(addToZero([ 1, 2, 3, 4, 5]))
 
 
 //2.Unique Characters
-
+//runtime O(n^2)
 function uniqueChars (str) {
+    let letters = ('')
     for (let i = 0; i< str.length; i++){
-        for ( let k = 1; k < str.length; k++){
-           if( str[i].includes(str[k])){
-            return true
-           }else{
+           if( letters.includes(str[i])){
             return false
+           }else{
+            letters += str[i]
            }
         }
+        return true
     }
-}
 
 
 // console.log(uniqueChars(`monday`))
 // console.log(uniqueChars(`moonday`))
 
 //.3 panagram sentance
-
+//runtime O(n^2)
 function isPanagram (str) {
-    let panagra = (`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`)
-   let panagram=panagra.split("")
-   let answer = []
-   let notAnswer = []
+    let panagram = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`]
     for (let i = 0 ; i<str.length; i++){
-        for (let k = 0; k<panagram.length; k++){
-            if(str[i] == panagram[k]){
-                answer.push()
+        for (let k = 0; k < panagram.length; k++){
+            if(str[i] === panagram[k] && i !== panagram[k]){
+                return true
             }else{
-                notAnswer.push(str[i])
+                
             }
         }
     }
-    return answer
+    return false
 }
 
 // console.log(isPanagram("The quick brown fox jumps over the lazy dog!"))
 // console.log(isPanagram("I like cats, but not mice"))
 
 //4. longest word
-
-function longestWord (word1, word2) {
-    let realWord1 = word1.split("")
-    let realWord2 = word2.split("")
-    for(let i = 0; i < realWord1.length; i++){
-        for (let k = 0; k< realWord2.length; k++){
-            if (realWord1[i]>realWord2[k]){
-                return indexOf(realWord1)
-            }else{
-                return indexOf(realWord2)
-            }
+// runtime O(n)
+function longestWord (arr) {
+    let answer = 0
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i].length > answer){
+            answer = arr[i].length
         }
     }
+    return answer
 }
 
-console.log(longestWord("hi", "hello"))
+console.log(longestWord(["hi", "hello"]))
